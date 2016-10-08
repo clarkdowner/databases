9691af87c1bcc918dc336321ad27596ee1d8001b
clarkdowner <cddowner@gmail.com>
2016-10-08 12:34:40 -0700

    fix duplicate user bug

--- server/models/index.js
+++ server/models/index.js
@@ -4,64 +4,40 @@ var Promise = require('bluebird');
 
 mySQL.connect();
 
+var queryForUserIdAsync = function(username) {
+  return new Promise((resolve, reject) => {
+    mySQL.query('select id from users where username = ?', [username], (err, userId) => {
+      if (err) { console.log(err); }
 
-// var queryForUserId = (username, cb) => {
-//   mySQL.query('select distinct id from users where username = ?', [username], (err, userId) => {
-//     module.exports.users.get(username, (err, username) => {
-//       if (err) {
-//         module.exports.users.post(username, () => cb(userId[0].id));
-//       } else {
-//         cb(userId[0].id);
-//       }
-//     });  
-//   });
-// };
-
-// var queryForRoomId = function(roomname, cb) {
-//   console.log('============================20==================');
-//   console.log('inside the room query', roomname);
-//   mySQL.query('select id from rooms where roomname = ?', [roomname], (err, roomId) => {
-//     module.exports.rooms.get(roomname, (err, results) => {
-//       if (err) {
-//         module.exports.rooms.post(roomname, () => cb(roomId[0].id));
-//       } else {
-//         cb(roomId[0].id);
-//       }
-//     });  
-//   });
-// };
-
-var queryForUserId = function(username) {
-  //return new Promise
-
-  mySQL.query('select id from users where username = ?', [username], (err, userId) => {
-    if (err) { console.log(err); }
-
-    cb(err, userId[0].id);
+      if (userId.length < 1) {
+        reject(err);
+      } else {
+        resolve(userId[0].id);
+      }
+    });
   });
 };
 
-var queryForUserIdAsync = Promise.promisify(queryForUserId);
+var queryForRoomIdAsync = function(roomname, cb) {
+  return new Promise((resolve, reject) => {
+    mySQL.query('select id from rooms where roomname = ?', [roomname], (err, roomId) => {
+      if (err) { console.log(err); }
 
-
-var queryForRoomId = function(roomname, cb) {
-  mySQL.query('select id from rooms where roomname = ?', [roomname], (err, roomId) => {
-    if (err) { console.log(err); } else {
-
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!', roomId);
-      cb(err, roomId[0].id);
-    }
+      if (roomId.length < 1) {
+        reject(err);
+      } else {
+        resolve(roomId[0].id);
+      }
+    });
   });
 };
 
-var queryForRoomIdAsync = Promise.promisify(queryForRoomId);
-
-var checkUser = function(username) {
+var findUserId = function(username) {
   return new Promise((resolve, reject) => {
     queryForUserIdAsync(username)
       .then((userId) => resolve(userId))
       .catch(() => {
-        module.exports.user.post(username, () => {
+        module.exports.users.post(username, () => {
           queryForUserIdAsync(username)
             .then((userId) => resolve(userId))
             .catch((err) => reject(err));
@@ -70,12 +46,12 @@ var checkUser = function(username) {
   });
 };
 
-var checkRoom = function(roomname) {
+var findRoomId = function(roomname) {
   return new Promise((resolve, reject) => {
     queryForRoomIdAsync(roomname)
       .then((roomId) => resolve(roomId))
       .catch(() => {
-        module.exports.room.post(roomname, () => {
+        module.exports.rooms.post(roomname, () => {
           queryForRoomIdAsync(roomname)
             .then((roomId) => resolve(roomId))
             .catch((err) => reject(err));
@@ -88,7 +64,6 @@ module.exports = {
   messages: {
     get: function (cb) {
       // return all msgs from the db.
-      // console.log('============================36==================');
       mySQL.query('select message from messages', (err, result) => {
         if (err) { throw err; }
 
@@ -96,9 +71,9 @@ module.exports = {
       });
     }, // a function which produces all the messages
     post: function (msg, cb) {
-      checkUser(msg.username)
+      findUserId(msg.username)
         .then((userId) => {
-          checkRoom(msg.roomname)
+          findRoomId(msg.roomname)
             .then((roomId) => {
               mySQL.query('insert into messages (message, user_id, room_id) values (?, ?, ?);', [msg.message, userId, roomId], (err) => {
                 if (err) { throw err; }
@@ -109,21 +84,6 @@ module.exports = {
             .catch((err) => console.log(err));
         })
         .catch((err) => console.log(err));
-
-
-
-      // queryForUserId(msg.username, (userId) => {
-      //   queryForRoomId(msg.roomname, (roomId) => {
-      //     console.log('============================47==================');
-      //     console.log('text', msg.message, 'user', userId, 'room', roomId);
-      //     mySQL.query('insert into messages (message, user_id, room_id) values (?, ?, ?);', [msg.message, userId, roomId], (err) => {
-      //       if (err) { throw err; }
-      //       console.log('message saved');
-      //       cb();
-      //     });
-      //   });
-      // });
-
     } // a function which can be used to insert a message into the database
   },
 
@@ -131,7 +91,6 @@ module.exports = {
     // Ditto as above.
     get: function (username, cb) {
       // return a user from the db.
-      // console.log('============================62==================');
       mySQL.query('select messages.message from messages inner join users on messages.user_id = users.id where users.username = ?;', [username], (err, result) => {
         if (err) { throw err; }
 
@@ -139,12 +98,18 @@ module.exports = {
       });
     },
     post: function (username, cb) {
+      queryForUserIdAsync(username)
+        .then(() => {
+          console.log('user already exists');
+          cb();
+        })
+        .catch(() => {
+          mySQL.query('insert into users (username) values (?)', [username], (err) => {
+            if (err) { throw err; }
+            cb();
+          });
+        });
       // add a new user to the db.
-      // console.log('============================71==================');
-      mySQL.query('insert into users (username) values (?)', [username], (err) => {
-        if (err) { throw err; }
-        cb();
-      });
     }
   },
 
@@ -152,7 +117,6 @@ module.exports = {
     // Ditto as above.
     get: function (roomname, cb) {
       // return a user from the db.
-      // console.log('============================83==================');
       mySQL.query('select messages.message from messages inner join rooms on messages.room_id = rooms.id where rooms.roomname = ?', [roomname], (err, result) => {
         if (err) { throw err; }
 
@@ -162,7 +126,6 @@ module.exports = {
     post: function (roomname, cb) {
       console.log('inside rooms put method', roomname);
       // add a new room to the db.
-      // console.log('============================92==================');
       mySQL.query('insert into rooms (roomname) values (?)', [roomname], (err) => {
         if (err) { throw err; }
         cb();
